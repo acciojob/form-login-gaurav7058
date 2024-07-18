360c31670f418e6b9487a279d4bab2bd2305f78a
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-18 14:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname = document.forms["form1"]["fname"].value;
+	let lname = document.forms["form1"]["lname"].value;
+	alert(fname+" "+lname);
+	
 }
